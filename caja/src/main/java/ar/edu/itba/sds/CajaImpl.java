6a65df47d67844736475c20c1d7fb7ed2cbe3ead
lpagn <lpagni@itba.edu.ar>
2020-05-18 11:04:48 -0300

    run

--- caja/src/main/java/ar/edu/itba/sds/CajaImpl.java
+++ caja/src/main/java/ar/edu/itba/sds/CajaImpl.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class CajaImpl implements Caja {
 
-	/*public Position position(int index) {
+	public Position position(int index) {
 		// TODO Auto-generated method stub
 		return null;
-	}*/
+	}
 
 	public boolean hasFreeSpace(int index) {
 		// TODO Auto-generated method stub
--- interfaces/src/main/java/ar/edu/itba/sds/Caja.java
+++ interfaces/src/main/java/ar/edu/itba/sds/Caja.java
@@ -3,7 +3,7 @@ package ar.edu.itba.sds;
 import java.util.List;
 
 public interface Caja {
-	//Position position(int index);
+	Position position(int index);
 	boolean hasFreeSpace(int index);
 	List<Integer> status();
 }
