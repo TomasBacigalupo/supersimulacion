39ad66c3c0a1e2cfb6e8d2a26d377ca7d6f9db2e
lpagn <lpagni@itba.edu.ar>
2020-05-22 10:11:41 -0300

    Puede llegar gente a la caja

--- caja/src/main/java/ar/edu/itba/sds/caja/App.java
+++ caja/src/main/java/ar/edu/itba/sds/caja/App.java
@@ -2,10 +2,14 @@ package ar.edu.itba.sds.caja;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 
 import ar.edu.itba.sds.model.Position;
 
 public class App {
+	
+	private static final double P = 0.8;
+	private static final int I = 10000000;
 	
     public static void main( String[] args ){
     	
@@ -13,8 +17,12 @@ public class App {
         System.out.println("Driver Class Caja Module\n");
         
         Position p = new Position(0,0);
-        CajaImpl caja = new CajaImpl(p,10,5,1,8);
+        CajaImpl caja = new CajaImpl(p,10,5,1,12);
         System.out.println(caja);
+        
+        Random rand = new Random();
+        
+        int i = 0;
         
         while(true) {
         
@@ -34,9 +42,22 @@ public class App {
 	        
 	        caja.add(70);
 	        
+	        caja.add(50);
+	        
+	        caja.add(60);
+	        
+	        caja.add(70);
+	        
 	        
 	        caja.print();
 	        while(!caja.isDone()) {
+	        	
+	        	double x = rand.nextDouble();
+	        	i++;
+	        	if(x > P && i%I==0) {
+	        		caja.add(80);
+	        	}
+	        	
 	        	caja.atender();
 	        }
 	        
